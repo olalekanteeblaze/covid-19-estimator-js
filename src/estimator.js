e66db2d70f82372d877ceb1f7480fd64e9821e9c
olalekanteeblaze <olalekanteeblaze@gmail.com>
2020-04-10 01:40:50 +0100

    frontend url

--- src/estimator.js
+++ src/estimator.js
@@ -23,7 +23,7 @@ const covid19ImpactEstimator = (data) => {
   const impactCurrentlyInfected = reportedCases * 10;
   const impactInfectionsByRequestedTime = impactCurrentlyInfected * (2 ** factor);
   const casesByRequestedTime = Math.floor(impactInfectionsByRequestedTime * 0.15);
-  const impactHospitalBedsByRequestedTime = (totalHospitalBeds * 0.35) - casesByRequestedTime;
+  const impactHospitalBedsByRequestedTime = Math.floor((totalHospitalBeds * 0.35) - casesByRequestedTime);
   const impactCasesForICUByRequestedTime = impactInfectionsByRequestedTime * 0.05;
   const impactCasesForVentilatorsByRequestedTime = impactInfectionsByRequestedTime * 0.02;
   const impactDollarsInFlight = impactInfectionsByRequestedTime * region.avgDailyIncomePopulation
@@ -33,7 +33,7 @@ const covid19ImpactEstimator = (data) => {
   const severeImpactCurrentlyInfected = reportedCases * 50;
   const severeImpactInfectionsByRequestedTime = severeImpactCurrentlyInfected * (2 ** factor);
   const severeCasesByRequestedTime = Math.floor(severeImpactInfectionsByRequestedTime * 0.15);
-  const hospitalBedsByRequestedTime = (totalHospitalBeds * 0.35) - severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime = Math.floor((totalHospitalBeds * 0.35) - severeCasesByRequestedTime);
   const casesForICUByRequestedTime = severeImpactInfectionsByRequestedTime * 0.05;
   const casesForVentilatorsByRequestedTime = severeImpactInfectionsByRequestedTime * 0.02;
   const dollarsInFlight = severeImpactInfectionsByRequestedTime * region.avgDailyIncomePopulation
